791e560a83d56d85223fd9e95604c4e85352bced
Volodymyr Petrenko <volodymyrpetrenko0@gmail.com>
2023-02-12 20:30:48 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,42 +6,46 @@ galleryEl.addEventListener('click', onClick);
 
 
 // разметка
-const markup = galleryItems.map((item) => `
-  <div class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
+
+const markup = galleryItems.map(item => makeMarkup (item)).join("");
+
+
+function makeMarkup ({preview, original, description}) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</div>`).join("");
+</div>`}
+
 
 galleryEl.insertAdjacentHTML("beforeend", markup);
 
 
+
+
+
 function onClick (evt) {
-    const currentTarget = evt.target;
+  const currentTarget = evt.target;
 
-    if (currentTarget.classList.contains('gallery__image')){
-        evt.preventDefault();
+  if (currentTarget.classList.contains('gallery__image')){
+      evt.preventDefault();
 
-        const instance = basicLightbox.create(`
-        <img src="${currentTarget.dataset.source}" width="800" height="600">`)
-        instance.show();
+      const instance = basicLightbox.create(`
+      <img src="${currentTarget.dataset.source}" width="800" height="600">`, {
+        onShow: document.addEventListener("keydown", onEscape),
+      });
+      instance.show();
 
-        // const rrr = document.querySelector(".basicLightbox")
-        // console.log(rrr);
-
-        document.addEventListener("keydown", (event) => {
-            console.log(event.key);
-            if (event.key === "Escape") {
-            instance.close(); 
-            }
-        });
-    };
+      function onEscape (event) {
+          if (event.key === "Escape") {
+          instance.close(); 
+          document.removeEventListener("keydown", onEscape)
+          }
+      };
+  };
 };
-
-
-
